7fa5c46bf9bd6c3eb33501db09ffecf802ecc0bf
j <joelsouthgate@googlemail.com>
2020-03-11 13:00:50 +0000

    comments and tidy

--- codetect/simdata.py
+++ codetect/simdata.py
@@ -29,7 +29,6 @@ class DataSimulator(ReadData):
         self.MU = MU
         self.PI = PI
         self.D = D
-#        assert D > 0
         # Simulate a population
         sys.stderr.write("Generating population\n")
         self.major, self.minor = self.gen_pair(GENOME_LENGTH, D)
@@ -40,15 +39,15 @@ class DataSimulator(ReadData):
         majorhams = [ham(self.major, s) for s in self.majorpop[0]]
         self._MAJOR = self.major
         self._MINOR = self.minor
-#        self.get_consensus() = self.major
         # Simulate reads
         sys.stderr.write("Simulating reads\n")
         self.COVWALK = self.random_coverage_walk(COVQ)
         self.X = self.sample_reads()
         # Parse data into a ReadData object
         super(DataSimulator,self).__init__(self.X,self._MAJOR)
+        # In case the number of reads has changed (some have been deleted:)
         self.N_READS = sum([Xi.count for Xi in self.X])
-        # Guarantee that the consensus has the highest
+        # Guarantee that the consensus has the highest for each
         for ci, c in enumerate(self.get_consensus()):
             assert max(self.M[ci]) == self.M[ci][c], (self.M[ci], c)
 
@@ -59,6 +58,12 @@ class DataSimulator(ReadData):
         return self._MAJOR
 
     def get_weight_base_array(self, T):
+        """
+        Get a weighted (by membership probability, or similar) frequency matrix
+
+        Args:
+            T: N_reads x 2 array of membership probabilites for 2 clusters
+        """
         baseweights = np.zeros((len(self.get_consensus()), 4))
         # FIRST CALCULATE THE MOST WEIGHTY BASE FOR EACH POSITION
         for k in range(len(self.V_INDEX)):
@@ -73,36 +78,16 @@ class DataSimulator(ReadData):
             baseweights[k] /= totalTk
         return baseweights
    
-    def debug_interval(self, emObj):
-        pi = emObj.pit
-        g = emObj.gt
-        T = emObj.Tt
-        st = emObj.st
-        # Calculate est pi
-
-        bw = self.get_weight_base_array(T)
-        for i in range(len(self.get_consensus())):
-            if st[i] == self.get_consensus()[i] and self.get_consensus()[i] != c2i[self.minor[i]]:
-                print(i, st[i], self.get_consensus()[i], c2i[self.minor[i]], self.M[i], bw[i])
-        z1reads = [Xi for Xi in self.X if Xi.z == 1]
-        z1M = np.zeros((len(self.get_consensus()), 4))
-        for Xi in z1reads:
-            for pos,b in Xi.get_aln():
-                z1M[pos,b] += 1
-        wba = self.get_weight_base_array(T)
-        pbweights = []
-        for i in range(len(self.V_INDEX)):
-            tmp = []
-            for ri in self.V_INDEX[i]:
-                tmp.append(T[ri,1])
-            pbweights.append(tmp)
-        for i,c in enumerate(st):
-            if st[i] != c2i[ds.minor[i]]:
-                print(i,sum([len(l) for l in ds.V_INDEX[i]]),[len(l) for l in ds.V_INDEX[i]],ds.get_consensus()[i], c2i[ds.minor[i]], ds.M[i], st[i], z1M[i], wba[i])
-                for pbw in pbweights[i]:
-                    print(pbw)
-
     def plot_genome(self,T,st):
+        """
+        Plot estimated coverage for each cluster across the genome,
+        respective posititions, estimated mutations,
+        and true values for each.
+
+        Args:
+            T: membership probability array per read
+            st: estimated sequence for alternative cluster
+        """
         cov0 = np.zeros(len(self.get_consensus()))
         cov1 = np.zeros(len(self.get_consensus()))
         for Xi in self.X:
@@ -114,7 +99,6 @@ class DataSimulator(ReadData):
                 for p,b in Xi.map.items():
                     cov1[p] += 1
         self.COV = cov0+cov1
-
         estcov0 = np.zeros(len(self.get_consensus()))
         estcov1 = np.zeros(len(self.get_consensus()))
         hamarr = np.zeros(len(self.get_consensus()))
@@ -127,12 +111,8 @@ class DataSimulator(ReadData):
 
         for i,Xi in enumerate(self.X):
             Ti = T[i]
-#            if Ti[0] > Ti[1]:
- #               for pos,base in Xi.get_aln():
             for pos,base in Xi.map.items():
                 estcov0[pos] += Ti[0]
-    #            else:
-     #               for pos,base in Xi.get_aln():
                 estcov1[pos] += Ti[1]
         plt.plot(hamarr, color='red', alpha=0.5)
         plt.plot(hamarr2, color='blue', alpha=0.5)
@@ -142,13 +122,11 @@ class DataSimulator(ReadData):
         plt.plot(estcov1,color='pink')
         plt.show()
 
-
     def debug_plot(self,emObj):
         """ Plot simulated data statistics for debugging.
     
         Args:
-            st: estimted string for minor species
-            T: T matrix from EM
+            emObj: an EM object used for parameter estimation
         """
         T = emObj.Tt
         st = emObj.st
@@ -158,13 +136,8 @@ class DataSimulator(ReadData):
         plt.bar(x=minorhams, height=self.minorpop[1])
         plt.title("minorhams")
         plt.show()
-
-
         for k in range(len(self.COV)):
             assert self.COV[k] == sum([len(l) for l in self.V_INDEX[k]])
-#        mc0 = max(cov0)
-#        hamarr *= mc0
-#        hamarr2 *= mc0
         nms = np.array([Xi.nm_major for Xi in self.X])
         plt.plot(self.COVWALK)
         plt.title("coverage_walk")
@@ -191,10 +164,14 @@ class DataSimulator(ReadData):
         plt.hist([r[0] for r in T])
         plt.title("T0")
         plt.show()
-#        print(T)
-
 
     def random_coverage_walk(self,covq):
+        """
+        Produce a random walk for simulating uneven coverage.
+        
+        Args:
+            covq: maximum fold coverage difference between minimum and maximum.
+        """
         xs = np.zeros(self.GENOME_LENGTH-self.READ_LENGTH+1)
         x = 0
         for i in range(len(xs)):
@@ -206,6 +183,13 @@ class DataSimulator(ReadData):
         return xs
 
     def mutate_perbase(self, seq, mutp):
+        """
+        Mutate the bases of a sequence independently.
+
+        Args:
+            seq: sequence to mutate.
+            mutp: probability a base will mutate.
+        """
         res = [c for c in seq]
         for i in range(len(res)):
             roll = random.uniform(0,1)
@@ -213,22 +197,36 @@ class DataSimulator(ReadData):
                 res[i] = random.choice([c for c in range(4) if res[i] != c])
         return res
 
-    def break_sticks(self, nsticks=100, alpha=5, min_middle=0.9):
-#        props = sorted(np.random.dirichlet([ for i in range(nsticks)]))
-        agg = min_middle
-        props = [min_middle]
+    def break_sticks(self, nsticks=100, alpha=5, q1=0.9):
+        """
+        Simulate a frequency distribution by stick-breaking.
+        
+        Optional args:
+            nsticks: dimension
+            alpha: beta distribution parameter
+            q1: probability of first stick
+        """
+        agg = q1
+        props = [q1]
         for n in range(nsticks):
             p = np.random.beta(1,alpha)
             props.append(p*(1-agg))
             agg += (p*(1-agg))
-        assert max(props) >= min_middle
+        assert max(props) >= q1
         props = sorted(props)
         assert sum(props) <= 1.0
         props[-1] += (1-sum(props))
-        assert max(props) >= min_middle, max(props)
+        assert max(props) >= q1, max(props)
         return props
 
     def gen_population(self, ref, mutp):
+        """
+        Generate a tight population of sequences by mutation and stick breaking.
+
+        Args:
+            ref: base string to mutate
+            mutp: per-base mutational probability
+        """
         # Guarantee the max pop has at least 50% of the mass
         props = sorted(self.break_sticks())
         assert sum(props) == 1, sum(props)
@@ -237,15 +235,33 @@ class DataSimulator(ReadData):
         return minorseqs + [ref], props
 
     def gen_pair(self,L,D):
+        """
+        Generate a pair of sequences representing the center of two clusters.
+
+        Args:
+            L: sequence length
+            D: Hamming distance between the two
+        """
         # Mutates by a fixed number
         major = [random.choice(range(4)) for i in range(self.GENOME_LENGTH)]
         minor = [c for c in major]
-        mutpos = np.random.choice(len(major), D, replace=False)
+            mutpos = np.random.choice(len(major), D, replace=False)
         for j in mutpos:
             minor[j] = random.choice([c for c in range(4) if c != major[j]])
         return major,minor
 
     def gen_aln(self,i,randpos, seq):
+        """ 
+        Generate a random read alignment.
+    
+        Args:
+            i: number.
+            randpos: random position of the alignment.
+            seq: base sequence.
+
+        Returns:
+            A ReadAln object.
+        """
         sampinds = [randpos+l for l in range(self.READ_LENGTH)]
         aln = ReadAln(i)
         for si in sampinds:
@@ -267,11 +283,8 @@ class DataSimulator(ReadData):
         for randpos in range(self.GENOME_LENGTH):
             for j in range(min_cov):
                 seqi = np.random.choice([0,1],p=w)
-             #   assert len(self.POPULATION) > 1
                 popseqs, popfreqs = pops[seqi]
                 seq = np.random.choice(popseqs, p=popfreqs)
-      #          randpos = np.random.randint(0,len(seq)-self.READ_LENGTH+1)
-    #            randpos = np.random.choice(range(len(seq)-self.READ_LENGTH+1), p=self.COVWALK)
                 aln = self.gen_aln(str(randpos)+str(j),randpos,seq)
                 aln.z = seqi
                 X.append(aln)
