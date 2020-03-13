38d61cc30b3e19acd3a8acb488e1251f1784db09
j <joelsouthgate@googlemail.com>
2020-03-13 10:59:42 +0000

    more assertions, documentation

--- codetect/likelihood_cache.py
+++ codetect/likelihood_cache.py
@@ -8,19 +8,22 @@ def logsumexp(logls):
     return m + np.log(sumo)        
 
 class LogLikelihoodCache():
-    def __init__(self, X, N, GL, GAMMA0, GAMMA1, CONSENSUS, PI, initstr):
+    def __init__(self, ds, N, GL, GAMMA0, GAMMA1, CONSENSUS, PI, initstr):
         self.N = N
         self.GL = GL
         self.wipe_memory()
         self.g1 = GAMMA1
         self.g0 = GAMMA0
         self.pi = PI
+        self.st = initstr
         self.initialized = False
         self.consensus = tuple([c for c in CONSENSUS])
-        self.nmarr = self.init_nmarr(X,initstr)
+        self.nmarr = None
+        self.nmarrFlag = False
+        self.init_nmarr(ds.X,initstr)
+        self.cal_full_loglikelihood(ds)
     def wipe_memory(self):
         self.initialized = False
-#        self.Larr = np.zeros((2,self.N,self.GL))
         self.Lsums0 = np.zeros(self.N)
         self.Lsums1 = np.zeros(self.N)
         self.Lsums = np.zeros(self.N)    
@@ -30,81 +33,65 @@ class LogLikelihoodCache():
         for i,xi in enumerate(X):
             nmarr[0,i] = xi.cal_ham(self.consensus)
             nmarr[1,i] = xi.cal_ham(altstr)
-        return nmarr
-    def update_nmarr(self,ds,j,bold,bnew):
+            assert nmarr[0,i] >= 0
+            assert nmarr[1,i] >= 0
+        self.nmarr = nmarr
+        self.nmarrFlag = True
+    def update_nmarr(self,ds,j,bnew):
+        assert bnew < 4
+        bold = self.st[j]
+        self.nmarrFlag = True
+        if bold == bnew:
+            return None
         assert bold != bnew
-        for i in ds.pos2reads(j): 
-            xi = ds.X[i]
-            if xi.map[j] == bold:
-                self.nmarr[1,i] += 1
-            elif xi.map[j] == bnew:
-                self.nmarr[1,i] -= 1
+        for i in ds.V_INDEX[j][bold]: 
+            self.nmarr[1,i] += 1
+        for i in ds.V_INDEX[j][bnew]:
+            self.nmarr[1,i] -= 1
+            assert self.nmarr[1,i] >= 0, (bold, bnew)
+        return None
     def cal_read_loglikelihood(self, ri, read, ci):
         q = self.g0
         if ci == 1:
             q = self.g1
+        assert self.nmarr[ci,ri] >= 0, self.nmarr[ci,ri]
         return np.log(1-q)*(read.get_length()-self.nmarr[ci,ri]) + np.log(q)*(self.nmarr[ci,ri])
-    def cal_full_loglikelihood(self, X, st):
-        for ri,read in enumerate(X):
+    def cal_full_loglikelihood(self, ds):
+        assert self.nmarrFlag
+        for ri,read in enumerate(ds.X):
             self.Lsums0[ri] = self.cal_read_loglikelihood(ri,read,0)
             self.Lsums1[ri] = self.cal_read_loglikelihood(ri,read,1)
-            self.Lsums[ri] = logsumexp([self.Lsums0[ri] + np.log(self.pi), self.Lsums1[ri] + np.log(1-self.pi)]) * X[ri].count
-#            print("read=",ri)
-#            print("pi=",self.pi)
-#            print("hams=",ham(self.consensus,st), ham(read, self.consensus), ham(read,st))
-#            print("logs=",self.Lsums0[ri], self.Lsums1[ri], self.Lsums[ri])
-#            print("condlike=",np.exp(self.Lsums0[ri])*(1-self.pi), np.exp(self.Lsums1[ri])*self.pi, self.Lsums[ri])
-#            print("likelihood=",np.exp(self.Lsums[ri]), np.exp(self.Lsums0[ri])*(1-self.pi) + np.exp(self.Lsums1[ri])*self.pi)
+            self.Lsums[ri] = logsumexp([self.Lsums0[ri] + np.log(self.pi), self.Lsums1[ri] + np.log(1-self.pi)]) * read.count
         self.initialized = True
         self.L = sum(self.Lsums)
+        assert self.L != None
         return self.L
-    def update_loglikelihood(self,X,i,b):
-        sumo = 0
+    def update_loglikelihood(self,ds,i,b):
 #        assert False
-        for ri, read in enumerate(X):
+#        assert b != self.st[i]
+        assert self.nmarrFlag
+        for ri in ds.pos2reads(i):
+            read = ds.X[ri]
             #** TODO: likelihood should not be recomputed for both here unless gamma has changed
             # 2X slower to do so
-            if i in read.map:
-                logp0 = self.getlogp(0,read.map[i],self.consensus[i])
-                self.Lsums0[ri] -= self.Larr[0,ri,i]
-                self.Larr[0,ri,i] = logp0
-                self.Lsums0[ri] += logp0             
-                #*****
-                logp1 = self.getlogp(1,read.map[i],b)
-                self.Lsums1[ri] -= self.Larr[1,ri,i]
-                self.Larr[1,ri,i] = logp1
-                self.Lsums1[ri] += logp1             
-                self.Lsums[ri] = logsumexp([self.Lsums0[ri] + np.log(self.pi), self.Lsums1[ri] + np.log(1-self.pi)])
-            sumo += self.Lsums[ri]*X[ri].count
-        self.L = sumo
-        return sumo             
-    def cal_PZ0s(self):
-        # Computes the log conditional probability for each datapoint
-        PZs = []
-        for ri, Li in enumerate(self.Lsums):       
-            numo = self.Lsums0[ri] + np.log(self.pi)
-            deno = self.Lsums[ri]
-#            print(numo,deno, np.exp(numo-deno))
-#            print(self.Lsums0[ri], self.Lsums1[ri])
-            PZs.append(np.exp(numo - deno))
-        return PZs
-    def point_estimate_pi(self):
-        probZ0s = self.cal_PZ0s()
-#        print(probZ0s, len(probZ0s))
-        newpi = sum(probZ0s)/len(probZ0s)
-        self.set_pi(newpi)
-        return newpi
-    def point_estimate_gamma(self,X):
-        probZ0s = self.cal_PZ0s()
-        spzs = sum(probZ0s)
-        numo = 0
-        for ri,r in enumerate(X):
-            numo += (probZ0s[ri] * r.cal_ham(self.consensus))
-#            print(probZ0s[ri] * ham(r, self.consensus))
-        newgam = numo/(spzs*GENOME_LENGTH)
-#        print(numo,spzs,newgam)
-        self.set_gamma(newgam)
-        return newgam
+            if read.map[i] == self.st[i]:
+                # TODO: compute log gamma once per round; dont need to log loads of times. slow.
+                self.Lsums0[ri] -= np.log(1-self.g0)
+                self.Lsums0[ri] += np.log(self.g0)
+                self.Lsums[ri] = logsumexp([self.Lsums0[ri] + np.log(self.pi), self.Lsums1[ri] + np.log(1-self.pi)]) * read.count
+            elif read.map[i] == b:
+                self.Lsums0[ri] -= np.log(self.g0)
+                self.Lsums0[ri] += np.log(1-self.g0) 
+                self.Lsums[ri] = logsumexp([self.Lsums0[ri] + np.log(self.pi), self.Lsums1[ri] + np.log(1-self.pi)]) * read.count
+        self.L = sum(self.Lsums)
+        self.st[i] = b
+        return self.L
+    def set_newb(self,ds,b,i):
+        assert b < 4
+        if self.st[i] != b:
+            self.nmarrFlag = False
+            self.st[i] = b
+            self.update_nmarr(ds,i,b)
     def set_g1(self,newg1):
         self.g1 = newg1
         self.wipe_memory()
@@ -114,18 +101,56 @@ class LogLikelihoodCache():
     def set_pi(self,newpi):
         self.pi = newpi
         self.wipe_memory()
-    def cal_loglikelihood(self,ds,st,i=None,bnew=None,bold=None,newpi=None,newg0=None,newg1=None):
+    def cal_loglikelihood(self,ds,i=None,newb=None,newpi=None,newg0=None,newg1=None,cache=True):
+        """ Calculate log likelihood. 
+
+        Interface to likelihood computation machinery. Takes a number of optional arguments,
+        and performs computation based on several cases.
+
+        Args:
+            rd: ReadData object
+
+        Optional Args:
+            i: position for new base
+            newb: new base at position i
+            cache: if false, recomputes the entire likelihood, otherwise makes use of speedups
+    
+        Returns:
+            Log likelihood of data given parameters.
+        """
+
+        #//*** Cases ***//
+        # Case 1: no caching, full recalculation required
+        if not cache:
+            self.wipe_memory()
+
+        # Case 2: new states have been proposed; any trigger full recalculation. Set state.
         if newg1 != None:
             self.set_g1(newg1)
         if newg0 != None:
             self.set_g0(newg0)
         if newpi != None:
             self.set_pi(newpi)
-        if bnew != None:
-            self.update_nmarr(ds,i,bold,bnew)
-        self.initialized=False
-        if not self.initialized:
-            return self.cal_full_loglikelihood(ds.X,st)
-        else:
-            return self.update_loglikelihood(ds.X,i,b)
 
+        # Case 3: no new parameters have been proposed that trigger full recalculation
+        if self.initialized:
+            # Case 3.a: no new parameters have been proposed at all; just return 
+            if None == newb == i:
+                assert self.L != None, "State has been wiped and likelihood requested without recalculation!"
+                return self.L
+            # Case 3.b: new base at position i has been proposed; fast recalculation performed
+            else:
+                # Case 3.b.1: new base at position i has been proposed but it is the same as the old one; don't recompute
+                if newb == self.st[i]:
+                    return self.L
+                self.set_newb(ds,newb,i)
+#                print("UPDATING FAST LIKELIHOOD")
+                return self.update_loglikelihood(ds,i,newb)
+
+        # Now perform a full recalculation
+        assert not self.initialized, "State is initialized but a full recomputation is requested! Bug."
+        assert newb == None
+#        print("CALCULATING FULL LIKELIHOOD")
+        res = self.cal_full_loglikelihood(ds)
+        assert self.L != None
+        return res
