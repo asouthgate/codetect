ac588f5f1e046f58079d58bd728f853750668703
j <joelsouthgate@googlemail.com>
2020-10-02 12:29:48 +0100

    bugfixes to satisfy linter

--- codetect/pycodetect/likelihood_calculator.py
+++ codetect/pycodetect/likelihood_calculator.py
@@ -1,3 +1,6 @@
+import numpy as np
+from pycodetect.utils import logsumexp
+
 class NMCache():
     """ Cache for storing and optimized updating the number of mismatches
         between reads and dynamic reference.
@@ -36,7 +39,7 @@ class NMCache():
         assert inc in [-1,1]
         self.nmarr[ci, ri] += inc
 
-class LikelihoodCalculator()
+class LikelihoodCalculator():
     """ 
     Optimized likelihood calculator using caching.
 
@@ -48,7 +51,7 @@ class LikelihoodCalculator()
         self.nm_cache = NMCache(read_aln_data, init_st)
         self.llcache = np.zeros(len(read_aln_data.X))
 
-    def logP_read(ri, read, ci, gamma, st, changed_bases=None):
+    def cal_logP_read(self, ri, read, ci, gamma, st, st_changed_bases=None):
         # TODO: should index of read be put inside read if they are being passed around like ids?
         """ Calculate log P(Aln | g,st) with caching.
 
@@ -59,18 +62,14 @@ class LikelihoodCalculator()
             gamma: gamma parameter.
             st: cluster string.
             changed_bases: (bi,b) tuples where st differs from the st of the last call.
-        """ 
-                """ Calculate Pr aln given that it belongs to minor group.
-        Args:
-            M: Lx4 categorical marginal distributions (sum(M[i]) = 1)
         """
         nothing_changed_for_read = True
-        if changed_inds is not None:
-            for bp, prevb in bases:
+        if st_changed_bases is not None:
+            for bp, prevb in st_changed_bases:
                 if bp in read.map:
                     c = read.map[bp]
                     if c == st[bp]:
-                        assert c != prevst[bp]
+                        assert c != prevb
                         # Previously it didnt match, now it does, so decrement the mismatch cache
                         nothing_changed_for_read = False
                         self.nm_cache.update(ci, ri, -1)
@@ -90,11 +89,11 @@ class LikelihoodCalculator()
             self.nm_cache.set(ci, ri, nm)
 
         if not nothing_changed_for_read:
-            matches = len(self.map) - self.nm_minor
-            self.llcache[ci, ri] = ( np.log(g) * self.nm_cache[ci, ri] ) + ( np.log(1-g) * matches )
+            matches = len(read.map) - self.nm_cache[ci, ri]
+            self.llcache[ci, ri] = ( np.log(gamma) * self.nm_cache[ci, ri] ) + ( np.log(1-gamma) * matches )
         return self.llcache[ci, ri]
 
-    def calc_log_likelihood(self, rd, st, g0, g1, pi):
+    def calc_data_log_likelihood(self, rd, st, g0, g1, pi, consensus, st_changed_bases):
         """
         Calculate the log likelihood of data given parameters.
 
@@ -112,14 +111,14 @@ class LikelihoodCalculator()
         sumo = 0
         for i, Xi in enumerate(rd.X):
             # TODO: replace read calculating its own LL with cache?
-            a = logP_read(g0)
-            b = logP_read(g1, st)
+            a = self.cal_logP_read(i, Xi, 0, g0, consensus, [])
+            b = self.cal_logP_read(i, Xi, 1, g1, st, st_changed_bases)
             lw1 = np.log(pi)
             lw2 = np.log(1-pi)
             sumo += ( logsumexp([a + lw1, b + lw2]) * Xi.count ) 
         return sumo
 
-    def calTi_pair2(self, Xi, pi, g0, g1, st, prev_st, changed_inds, consensus):
+    def cal_P_cluster_given_read(self, ri, Xi, pi, g0, g1, st, consensus, st_changed_bases):
         """ Calculate the ith membership conditional probability array element.
         
         Args:
@@ -128,18 +127,16 @@ class LikelihoodCalculator()
             g0: gamma parameter for cluster 0
             st: cluster 1 string
             g1: gamma parameter for cluster 1
-            prev_st: previous cluster 1 string
-            changed_inds: indices such that st[i] != prev_st[i]        
+            consensus: consensus used for cluster 0 string
+            st_changed_bases: ind, base tuples such that st[ind] != base
 
         Returns:
             tp: T array: P(Z_i = k | X_i)
             lp: Likelihood of Xi given cluster j
         """
-        if changed_inds is None:
-            assert st == prev_st
 
-        l1 = logP_read(ri, Xi, ci, g0, consensus, consensus, [])
-        l2 = logP_read(ri, Xi, ci, g1, st, prev_st, changed_inds)
+        l1 = self.cal_logP_read(ri, Xi, 0, g0, consensus, [])
+        l2 = self.cal_logP_read(ri, Xi, 1, g1, st, st_changed_bases)
 
         lw1 = np.log(pi)
         lw2 = np.log(1-pi)
@@ -148,7 +145,7 @@ class LikelihoodCalculator()
         exp2 = np.exp(l2 + lw2)
 
         c = exp1 + exp2
-        assert 0 < c <= 1.01:
+        assert 0 < c <= 1.01
 
         t1i = exp1/c
         t2i = exp2/c
