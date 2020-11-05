22609de8e65ccb17bc3568f5f7d06967ce6716ee
Cloud User <centos@flu-assembler2.novalocal>
2020-11-05 09:44:08 +0000

    remove old null call; output sequence header as well

--- codetect/codetectem.py
+++ codetect/codetectem.py
@@ -17,6 +17,7 @@ from pycodetect.read_aln_data import ReadAlnData
 from pycodetect.utils import str_c2i, str_i2c, str_only_ACGT
 from pycodetect.plotter import plot_mask
 from pycodetect.ref_panel import RefPanel
+import numpy as np
 import argparse
 
 
@@ -51,25 +52,25 @@ if __name__ == "__main__":
 #[t, self.calc_log_likelihood(st,gt,mut,pit), pit, gt, mut
     if not args.debug_minor:
         if args.ref_msa is None:
-            trace = em.estimate()
+            trace, refh = em.estimate()
         else:
             rp = RefPanel(em.consensus, args.ref_msa, ref_rec.description, min_d=args.mind)
-            trace = em.estimate(ref_panel=rp)
+            trace, refh = em.estimate(ref_panel=rp)
     else:
         dbm = [str_c2i(str_only_ACGT(str(r.seq))) for r in SeqIO.parse(args.debug_minor, "fasta")][0] 
         if args.ref_msa is None:
             sys.stderr.write("Running without ref panel\n")
-            trace = em.estimate(debug_minor=dbm,debug=True)
+            trace, refh = em.estimate(debug_minor=dbm,debug=True)
         else:
             sys.stderr.write("running with ref panel\n")
             rp = RefPanel(em.consensus, args.ref_msa, ref_rec.description, min_d=args.mind)
-            trace = em.estimate(ref_panel=rp,debug_minor=dbm,debug=True)
+            trace, refh = em.estimate(ref_panel=rp,debug_minor=dbm,debug=True)
 
 #    L0 = em.calc_L0()
     sys.stderr.write("Calculating H0\n")
-    L0 = em.calc_L0()
-    sys.stderr.write("L0: %f\n" % L0)
-    alt_trace = em.estimate(min_pi=1.0,fixed_st=trace[-1][-1])
+#    L0 = em.calc_L0()
+#    sys.stderr.write("L0: %f\n" % L0)
+    alt_trace, refhnull = em.estimate(min_pi=1.0,fixed_st=trace[-1][-1])
     nsites = len(em.rd.VALID_INDICES)
     with open(args.out+".summary.csv", "w") as f:
         f.write("nsites\n%d" % (nsites))
@@ -80,6 +81,6 @@ if __name__ == "__main__":
         for line in alt_trace:
             f.write(",".join([str(s) for s in line][:-1])+"\n")
     with open(args.out+".est.fa", "w") as f:
-        f.write(">1\n%s" % str_i2c(trace[-1][-1]) )
+        f.write(">%s\n%s" % (refh, str_i2c(trace[-1][-1]) ))
 
 
